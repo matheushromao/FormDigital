b9203ea151013d44ec9caae276a14a756a9d0507
Matheus Romão <matheus_henrique_9@hotmail.com>
2026-07-22 19:53:52 -0300

    inicio do projeto FormDigital

---
+++ src/main/java/com/formdigital/FormDigital/FormDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.formdigital.FormDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formdigital/FormDigital/FormDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formdigital.FormDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
